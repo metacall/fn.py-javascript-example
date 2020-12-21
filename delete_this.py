626af8b3ba640d76c931900344ad8809c87d8a5a
viferga <vic798@gmail.com>
2020-12-21 13:38:23 -0900

    Example working properly but still using a wrapper to handle the import of py functions.

---
+++ delete_this.py
@@ -0,0 +1,2 @@
+# TODO: This must be removed and imported directly form NodeJS
+from fn.op import foldr, call
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-const { foldr, call } = require('fn.op.py');
+require('metacall');
+const { foldr, call } = require('delete_this.py');
+
+// TODO: Import directly the Python package instead of a wrapper
+// const { foldr, call } = require('fn.op');
 
 console.log(foldr(call, 10)([s => s * s, k => k + 10]));
