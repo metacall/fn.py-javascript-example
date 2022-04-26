0ce209b889738b7c94d7cee8cf6429dbf7ba23af
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2022-04-26 22:02:44 +0200

    Add more examples.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // Require the 'op' submodule from 'fn' Python package
-const { foldr, call } = require('fn.op');
+const { foldr, call, apply, flip } = require('fn.op');
 
 // Invoke Python functions (it allows function composition between languages)
-console.log(foldr(call, 10)([s => s * s, k => k + 10]));
+console.log(foldr(call, 10)([s => s * s, k => k + 10])); // 400
+
+const add = (a, b) => a + b;
+const sub = (a, b) => a - b;
+
+console.log(apply(add, [1, 2])); // 3
+console.log(flip(sub)(20,10)); // -10
